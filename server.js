318bc715827b1503918cf16bfe91a61170ad4d37
Marcos Alexandre <marcosalexandrejlc@gmail.com>
2025-02-04 13:29:59 -0300

    mongoDB

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config
+const express = require('express');
+const app = express();
+const moongoose = require('mongoose')
+moongoose.connect(process.env.CONNETIONSTRING)
+.then(() => {
+  app.emit('pronto')
+})
+.catch(e => console.log(e))
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middleware/middleware');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Nossos próprios middlewares
+app.use(middlewareGlobal);
+app.use(routes);
+app.on('pronto', ()=> {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
+})
