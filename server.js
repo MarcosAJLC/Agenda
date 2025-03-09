1811c9f31865f16c757f37f45b9e1ad6dd9f00be
MarcosAJLC <marcosalexandrejlc@gmail.com>
2025-03-09 10:55:23 -0300

    Agenda

--- server.js
+++ server.js
@@ -1,49 +1,59 @@
-require('dotenv').config()
+require('dotenv').config();  // Carregar variáveis de ambiente
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const moongoose = require('mongoose')
-const connetionString = process.env.MONGODB_URI;
-moongoose.connect(connetionString)
-  .then(() => {
-    app.emit('pronto')
-  })
-  .catch(e => console.log(e))
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const flash = require('connect-flash')
-const helmet = require('helmet')
-const routes = require('./routes');
 const path = require('path');
-const csfr = require('csurf')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const helmet = require('helmet');
+const routes = require('./routes');
+const csfr = require('csurf');
 const { middlewareGlobal, checkCSRF404, csrfMiddleware } = require('./src/middleware/middleware');
-app.use(helmet())
+
+// Conectar ao MongoDB
+const connectionString = process.env.MONGODB_URI;
+
+mongoose.connect(connectionString)
+  .then(() => {
+    console.log("Conectado ao MongoDB");
+    app.emit('pronto');
+  })
+  .catch((err) => {
+    console.error("Erro de conexão ao MongoDB:", err);
+  });
+
+// Configurações do Express
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.resolve(__dirname, 'public')));
-const sessionsopt = session({
-  secret: 'Bem vindo!',
-  store: MongoStore.create({ mongoUrl: 'mongodb+srv://MarcosAlexandre:emigam2008@cluster0.9ww0n.mongodb.net/Database' }),
-  
-
-  // mongodb+srv://MarcosAlexandre:emigam2008@cluster0.9ww0n.mongodb.net/Database?retryWrites=true&w=majority&appName=Cluster0
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'segredoPadrao',
+  store: MongoStore.create({ mongoUrl: connectionString }),
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true
-  }
-})
-app.use(sessionsopt)
-app.use(flash())
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 7, httpOnly: true }
+}));
+
+// Configuração de EJS
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
-app.use(csfr())
+
+// Middleware de CSRF
+app.use(csfr());
 app.use(middlewareGlobal);
 app.use(checkCSRF404);
-app.use(csrfMiddleware)
+app.use(csrfMiddleware);
+
+// Definindo as rotas
 app.use(routes);
+
+// Quando o MongoDB estiver pronto, inicie o servidor
 app.on('pronto', () => {
+  const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
+  });
 });
-})
