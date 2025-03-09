a763d8a70f6d6aa2acad4396ca4008085107be67
MarcosAJLC <marcosalexandrejlc@gmail.com>
2025-03-09 09:57:09 -0300

    Agenda

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,9 +1,12 @@
-const ContatoM = require('../models/contatomodel')
+const ContatoM = require('../models/contatomodel');
+const mongoose = require('mongoose');
+
 exports.index = (req, res) => {
   res.render('contato', {
     contato: {}
   });
-}
+};
+
 exports.register = async (req, res) => {
   try {
     const contat = new ContatoM(req.body);
@@ -16,29 +19,44 @@ exports.register = async (req, res) => {
     }
 
     req.flash('success', 'Contato registrado com sucesso.');
-    req.session.save(() => res.redirect(`/contato/index/${req.params.id}`));
+    req.session.save(() => res.redirect(`/contato/index/${contat.contato._id}`));
     return;
   } catch (e) {
-    console.log(e);
+    console.error(e);
     return res.render('404');
   }
 };
+
 exports.edit = async function (req, res) {
-  if (!req.params.id) return res.render('404');
+  const { id } = req.params;
 
-  const contato = await ContatoM.Busca(req.params.id);
-  if (!contato) return res.render('404');
+  if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+    console.error("ID inválido:", id);
+    return res.render('404');
+  }
 
-  res.render('contato', { contato });
+  try {
+    const contato = await ContatoM.Busca(id);
+    if (!contato) return res.render('404');
+
+    res.render('contato', { contato });
+  } catch (error) {
+    console.error(error);
+    return res.render('404');
+  }
 };
 
 exports.editCont = async function (req, res) {
   try {
-    if(!req.params.id) return res.render('404');
-    const contato = new ContatoM(req.body);
-    await contato.editCont(req.params.id);
+    const { id } = req.params;
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+      return res.render('404');
+    }
 
-    if(contato.errors.length > 0) {
+    const contato = new ContatoM(req.body);
+    await contato.editCont(id);
+
+    if (contato.errors.length > 0) {
       req.flash('errors', contato.errors);
       req.session.save(() => res.redirect(`/`));
       return;
@@ -47,16 +65,21 @@ exports.editCont = async function (req, res) {
     req.flash('success', 'Contato editado com sucesso.');
     req.session.save(() => res.redirect(`/`));
     return;
-  } catch(e) {
-    console.log(e);
+  } catch (e) {
+    console.error(e);
     res.render('404');
   }
-}
-exports.delete = async function(req, res) {
-  if(!req.params.id) return res.render('404');
+};
 
-  const contato = await ContatoM.delete(req.params.id);
-  if(!contato) return res.render('404');
+exports.delete = async function (req, res) {
+  const { id } = req.params;
+
+  if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+    return res.render('404');
+  }
+
+  const contato = await ContatoM.delete(id);
+  if (!contato) return res.render('404');
 
   req.flash('success', 'Contato apagado com sucesso.');
   req.session.save(() => res.redirect('/'));
